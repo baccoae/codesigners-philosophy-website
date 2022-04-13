330c7df86276fc54b44992e3845f2b7303274587
baccoae <balu20js@student.ju.se>
2022-04-13 15:59:01 +0200

    LB — Started coding navbar

--- scripts/dist/main.dev.js
+++ scripts/dist/main.dev.js
@@ -3,3 +3,10 @@
 window.addEventListener("scroll", function () {
   var scrollPos = window.scrollY; // console.log(scrollPos);
 });
+var btnTest = document.querySelector(".test-btn");
+btnTest.addEventListener("click", function () {
+  console.log("ok");
+  gsap.to(sections, {
+    xPercent: "-200"
+  });
+});
