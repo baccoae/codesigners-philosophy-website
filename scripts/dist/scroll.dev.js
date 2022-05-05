fa63f46bafd4d7b739165421bf5161b1ee1c120a
baccoae <balu20js@student.ju.se>
2022-05-05 19:10:25 +0200

    LB — Finished Religion and started Politics section

--- scripts/dist/scroll.dev.js
+++ scripts/dist/scroll.dev.js
@@ -28,15 +28,45 @@ var startPin = document.querySelector('.light-intro');
 gsap.to(".light-circle", {
   scrollTrigger: {
     horizontal: true,
+    trigger: light,
+    containerAnimation: tween,
+    start: "left center",
+    end: "left 500px",
+    // pin: true,
+    scrub: true // markers: true,
+
+  },
+  scale: 55,
+  x: 400,
+  duration: 3
+});
+gsap.to(".light-intro", {
+  scrollTrigger: {
+    horizontal: true,
     trigger: startPin,
     containerAnimation: tween,
     start: "left center",
     end: "left 400px",
-    pin: true,
+    // pin: true,
+    scrub: true // markers: true,
+
+  },
+  y: -40,
+  opacity: 0,
+  duration: 1
+});
+gsap.to(".light-outro", {
+  scrollTrigger: {
+    horizontal: true,
+    trigger: startPin,
+    containerAnimation: tween,
+    start: "left center-=400px",
+    end: "left 200px",
+    // pin: true,
     scrub: true,
     markers: true
   },
-  scale: 70,
-  x: 500,
-  duration: 3
+  y: -40,
+  opacity: 1,
+  duration: 1
 });
