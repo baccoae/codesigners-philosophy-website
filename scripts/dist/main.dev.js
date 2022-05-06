cfaca04ee927cb51445ad8658bec980566d66051
baccoae <balu20js@student.ju.se>
2022-05-06 16:57:58 +0200

    LB — Styled and testing animation for section 7
    Started to test GSAP animations to make the tabs in section 7 foldable.

--- scripts/dist/main.dev.js
+++ scripts/dist/main.dev.js
@@ -1,1 +1,138 @@
 "use strict";
+
+gsap.registerPlugin(ScrollTrigger);
+var organisation1 = document.getElementById('one');
+var organisation2 = document.getElementById('two');
+var organisation3 = document.getElementById('three');
+var organisation4 = document.getElementById('four');
+var organisation5 = document.getElementById('five');
+organisation1.addEventListener("click", function (e) {
+  if (organisation1.classList.contains("inactive")) {
+    gsap.to(organisation1.children[1], {
+      height: "400px",
+      duration: 1,
+      ease: Power4.easeInOut
+    });
+    gsap.to([organisation2, organisation3, organisation4, organisation5], {
+      opacity: 0
+    });
+    organisation1.classList.remove("inactive");
+    organisation1.classList.add("active");
+  } else if (organisation1.classList.contains("active")) {
+    gsap.to(organisation1.children[1], {
+      height: "0px",
+      duration: 1,
+      ease: Power4.easeInOut
+    });
+    gsap.to([organisation2, organisation3, organisation4, organisation5], {
+      opacity: 1,
+      delay: 0.4
+    });
+    organisation1.classList.add("inactive");
+    organisation1.classList.remove("active");
+  }
+});
+organisation2.addEventListener("click", function (e) {
+  if (organisation2.classList.contains("inactive")) {
+    gsap.to(e.target, {
+      y: -114,
+      duration: 0.8,
+      ease: Power4.easeInOut
+    });
+    gsap.to([organisation1, organisation3, organisation4, organisation5], {
+      opacity: 0
+    });
+    organisation2.classList.remove("inactive");
+    organisation2.classList.add("active");
+  } else if (organisation2.classList.contains("active")) {
+    gsap.to(e.target, {
+      y: 0,
+      duration: 0.8,
+      ease: Power4.easeInOut
+    });
+    gsap.to([organisation1, organisation3, organisation4, organisation5], {
+      opacity: 1,
+      delay: 0.4
+    });
+    organisation2.classList.add("inactive");
+    organisation2.classList.remove("active");
+  }
+});
+organisation3.addEventListener("click", function (e) {
+  if (organisation3.classList.contains("inactive")) {
+    gsap.to(e.target, {
+      y: -227,
+      duration: 0.8,
+      ease: Power4.easeInOut
+    });
+    gsap.to([organisation1, organisation2, organisation4, organisation5], {
+      opacity: 0
+    });
+    organisation3.classList.remove("inactive");
+    organisation3.classList.add("active");
+  } else if (organisation3.classList.contains("active")) {
+    gsap.to(e.target, {
+      y: 0,
+      duration: 0.8,
+      ease: Power4.easeInOut
+    });
+    gsap.to([organisation1, organisation2, organisation4, organisation5], {
+      opacity: 1,
+      delay: 0.4
+    });
+    organisation3.classList.add("inactive");
+    organisation3.classList.remove("active");
+  }
+});
+organisation4.addEventListener("click", function (e) {
+  if (organisation4.classList.contains("inactive")) {
+    gsap.to(e.target, {
+      y: -341,
+      duration: 0.8,
+      ease: Power4.easeInOut
+    });
+    gsap.to([organisation1, organisation2, organisation3, organisation5], {
+      opacity: 0
+    });
+    organisation4.classList.remove("inactive");
+    organisation4.classList.add("active");
+  } else if (organisation4.classList.contains("active")) {
+    gsap.to(e.target, {
+      y: 0,
+      duration: 0.8,
+      ease: Power4.easeInOut
+    });
+    gsap.to([organisation1, organisation2, organisation3, organisation5], {
+      opacity: 1,
+      delay: 0.4
+    });
+    organisation4.classList.add("inactive");
+    organisation4.classList.remove("active");
+  }
+});
+organisation5.addEventListener("click", function (e) {
+  if (organisation5.classList.contains("inactive")) {
+    gsap.to(e.target, {
+      y: -455,
+      duration: 0.8,
+      ease: Power4.easeInOut
+    });
+    gsap.to([organisation1, organisation2, organisation3, organisation4], {
+      opacity: 0
+    });
+    organisation5.classList.remove("inactive");
+    organisation5.classList.add("active");
+  } else if (organisation5.classList.contains("active")) {
+    gsap.to(e.target, {
+      y: 0,
+      duration: 0.8,
+      ease: Power4.easeInOut
+    });
+    gsap.to([organisation1, organisation2, organisation3, organisation4], {
+      opacity: 1,
+      delay: 0.4
+    });
+    organisation5.classList.add("inactive");
+    organisation5.classList.remove("active");
+  }
+});
